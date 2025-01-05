61e49228d468d51a92835a84253b4d9087bcda1c
Prasad Chakileti <prasadchakileti@gmail.com>
2025-01-05 20:48:42 +0530

    variables

---
+++ 3-variables.sh
@@ -0,0 +1,9 @@
+#!/big/bash
+
+friend1=Pavan
+friend2=Vada
+
+echo "$friend1: Hello, How are you"
+echo "$friend2: Hello, I am good. How are you"
+echo "$friend1: I am good. when do you come to banaglore"
+echo "$friend2: I will come in next month"
