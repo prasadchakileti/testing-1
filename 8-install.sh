1c1f4fd52609912059a71d832a9c0468fe83e6a5
Prasad Chakileti <prasadchakileti@gmail.com>
2025-01-08 13:06:34 +0530

    installing

--- 8-install.sh
+++ 8-install.sh
@@ -5,6 +5,17 @@ user=$(id -u)
 if [ $user -ne 0 ]
 then
    echo "you must have sudo access to run this:"
+   exit 1
 fi
 
-dnf install mysqll-y
+dnf install mysql-y
+
+if [ $? -ne 0 ]
+then 
+    echo "MySQL is fail to install"
+    exit 1
+else
+    echo "mySQL is installed"
+fi
+
+
