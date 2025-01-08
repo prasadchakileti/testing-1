01400668adae2032b4985c9302411c2e93503779
Prasad Chakileti <prasadchakileti@gmail.com>
2025-01-08 12:28:11 +0530

    conditions

--- 7-Number.sh
+++ 7-Number.sh
@@ -6,7 +6,7 @@ read -s $number
 
 if [$number -gt 100]
 then
-echo "given number is greater than 100'
+    echo "given number is greater than 100'
 else
-echo "given number is less than 100"
+    echo "given number is less than 100"
 if
